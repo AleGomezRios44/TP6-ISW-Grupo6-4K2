d0c5d570386b61340eb6c629bd9ce40f56976d2f
AleGomezRios44 <nicogr0809@gmail.com>
2024-09-15 16:28:39 -0300

    Archivo de configuración

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,22 +2,23 @@ import { initializeApp } from "firebase/app";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { v4 } from "uuid";
 
+// Configuración de Firebase con variables de entorno
 const firebaseConfig = {
-  apiKey: "AIzaSyCMAFfS_ENkUsPI27kpYOubRjkEPszj7IQ",
-  authDomain: "tp6-tangoapp.firebaseapp.com",
-  projectId: "tp6-tangoapp",
-  storageBucket: "tp6-tangoapp.appspot.com",
-  messagingSenderId: "322992535399",
-  appId: "1:322992535399:web:1fe04cfbe293b538a3a1c2",
-  measurementId: "G-SXHKBL4VM8"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
 
 export const uploadFile = async (file) => {
-    const storageRef = ref(storage, v4())
-    await uploadBytes(storageRef, file)
-    const url = await getDownloadURL(storageRef)
-    return url
-}
+    const storageRef = ref(storage, v4());
+    await uploadBytes(storageRef, file);
+    const url = await getDownloadURL(storageRef);
+    return url;
+};
